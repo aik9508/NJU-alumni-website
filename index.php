3b71c05b0638da5f7c701d9df41603bc6ee3a633
Shiwen XIA <Shiwen XIA>
2017-02-16 05:38:37 +0100

    First simple version with only header and footer.

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/personal.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <title>Bienvenues</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div id="logo" class="col-lg-3">
+                <a href="index.php"><img src="sources/logo.png"></a>
+            </div>
+            <div class="header-right col-lg-4">
+                <div class="glyphicon glyphicon-user"></div>
+                <div class="glyphicon glyphicon-globe"></div>
+            </div>
+        </div>
+    </header>
+    <div id="menu" class="collapse navbar-collapse">
+        <div class="container">
+            <ul class="nav navbar-nav">
+                <li class="menu-item">NOTRE PROFIL</li>
+                <li class="menu-item">ACTIVITÉS</li>
+                <li class="menu-item">COMMUNAUTÉ</li>
+                <li class="menu-item">CONTACTEZ-NOUS</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="mainbody">
+        <div class="jumbotron col-lg-12">
+            Here is the big picture
+        </div>
+        <div class="activities col-lg-7">
+            Here is activities
+        </div>
+        <div class="announce col-lg-4">
+            Here is announces
+        </div>
+    </div>
+
+
+
+
+    <footer>
+        <div class="footer-top container">
+            <div class="friendly-links col-lg-4">
+                <img src="sources/icon.png">
+            </div>
+            <div class="foot-logo col-lg-4">
+                <img src="sources/foot_logo.png">
+            </div>
+            <div class="follow-links col-lg-4">
+                <img src="sources/shares.png">
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>Copyright</p>
+        </div>
+    </footer>
+
+</body>
+</html>
