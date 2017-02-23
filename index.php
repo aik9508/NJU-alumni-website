f371a2953603f4fe3a2bba887e907daa561a1035
Shiwen XIA <Shiwen XIA>
2017-02-23 08:29:43 +0100

    I have writen the markup of home page, welcome page, and profile page.

--- index.php
+++ index.php
@@ -8,23 +8,25 @@
     <link rel="stylesheet" href="css/master.css">
     <link rel="stylesheet" href="css/personal.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
-    <script type="text/javascript" src="js/jquery.js"></script>
-    <script src="js/BBS.js"></script>
-    <title>Bienvenues</title>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/ajax-utils.js"></script>
+
 </head>
 <body>
 <header>
     <div class="container">
         <div class="row">
             <div id="logo" class="col-lg-3">
-                <a href="index.php"><img src="sources/logo.png"></a>
+                <a href="index.php"><img src="images/logo.png"></a>
             </div>
             <div id="association" class="col-lg-3">
                 <span>法国校友会</span>
             </div>
             <div class="header-right col-lg-2">
-                <div class="glyphicon glyphicon-user"></div>
-                <div class="glyphicon glyphicon-globe"></div>
+                <div id="button-inscription" class="glyphicon glyphicon-user"></div>
+                <div id="button-language" class="glyphicon glyphicon-globe"></div>
             </div>
         </div>
     </div>
@@ -32,131 +34,21 @@
 <div id="menu" class="collapse navbar-collapse">
     <div class="container">
         <ul class="nav navbar-nav">
-            <li class="menu-item">ACCUEIL</li>
-            <li class="menu-item">NOTRE PROFIL</li>
-            <li class="menu-item">ACTIVITÉS</li>
-            <li class="menu-item">COMMUNAUTÉ</li>
-            <li class="menu-item">CONTACTEZ-NOUS</li>
+            <li id="button-accueil" class="menu-item">ACCUEIL</li>
+            <li id="button-profile" class="menu-item">NOTRE PROFIL</li>
+            <li id="button-activity" class="menu-item">ACTIVITÉS</li>
+            <li id="button-community" class="menu-item">COMMUNAUTÉ</li>
+            <li id="button-contact" class="menu-item">CONTACTEZ-NOUS</li>
         </ul>
     </div>
 </div>
 
-<!--start home snippet-->
-<!--最好写一个函数generateJumbotron来生成-->
-<div class="jumbotron">
-    <div class="container">
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
+<!-- Here insert main contents of each page -->
+<div id="main-content">
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img src="sources/jumbotron1.png" alt="picture 1" class="img-responsive">
-                </div>
+</div><!--Here ends the main contents-->
 
-                <div class="item">
-                    <img src="sources/jumbotron2.png" alt="picture 2" class="img-responsive">
-                </div>
-
-                <div class="item">
-                    <img src="sources/jumbotron3.png" alt="picture 3" class="img-responsive">
-                </div>
-            </div>
-
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </div>
-</div>
-
-<!--Here comes the main contents-->
-<div id="main-content" class="container">
-    <div class="row">
-        <div id="activities" class="block-tile col-lg-8 col-md-8 col-sm-7 col-xs-12">
-            <div id="activity-title">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span>ACTIVITÉS</span>
-            </div>
-            <div class='hr bighr'></div>
-
-            <?php
-            $nbrOfExp = 5;
-            for ($i = 1; $i <= $nbrOfExp; $i++) {
-                $greyBgd = ($i % 2 == 0) ? '' : 'greybgd';
-                $date = '[01-10-2016]';
-                echo <<<EOT
-                    <div class="$greyBgd">
-                        <div class="row">
-                            <div class="activity-thumnail col-md-3 col-sm-3">
-                                <img class="img-responsive" src="sources/homeface1_tn.jpg" alt="activity thumnail">
-                            </div>
-                            <div class="activity-description col-md-9 col-sm-9">
-                                <p>
-                                    BlablabalabalablabalbalablabalbaklablabalalkdkgkahgjdajflkjwoirhfgoahdkafakljfklajflBlablabalabalablabalbalablabalbaklablabalalkdkgkahgjdajflkjwoirhfgoahdkafakljfklajflBlablabalabalablabalbalablabalbaklablabalalkdkgkahgjdajflkjwoi
-                                </p>
-                                <p>$date</p>
-                            </div>
-                        </div>
-                    </div>
-EOT;
-            }
-            echo "<div class='hr bighr'></div>";
-            ?>
-
-        </div><!--End of activities block-->
-
-        <div id="information" class="block-tile col-lg-4 col-md-4 col-sm-5 col-xs-12">
-            <div id="information-title">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span>INFORMATIONS</span>
-            </div>
-            <div class='hr bighr'></div>
-
-            <?php
-            $nbrOfExp = 10;
-            for ($i = 1; $i <= $nbrOfExp; $i++) {
-                $greyBgd = ($i % 2 == 0) ? '' : 'greybgd';
-                echo <<<EOT
-                    <div class="information-tile $greyBgd">
-                        <span  class="glyphicon glyphicon-paperclip"></span>
-                        <span>
-                            Une phrase de titre d'information
-                        </span>
-                    </div>
-EOT;
-            }
-            echo "<div class='hr bighr'></div>";
-            ?>
-
-        </div><!--End of information block-->
-
-        <div id="statistic" class="block-tile col-lg-4 col-md-4 col-sm-5 col-xs-12">
-            <?php
-            $alumnis = 1043;
-            $visitors = 2625;
-            $nbrOfAlumnis = number_format($alumnis,0,',',' ');
-            $nbrOfVisitors = number_format($visitors,0,',',' ');
-            echo "<div id='alumni-number'><strong class='count-number'>$nbrOfAlumnis</strong><span class='count-name'>&nbsp;&nbsp;Alumnis</span></div>";
-            echo "<div id='visitor-number'><strong class='count-number'>$nbrOfVisitors</strong><span class='count-name'>&nbsp;&nbsp;Visiteurs</span></div>";
-            ?>
-        </div>
-    </div>
-    <!--End of home snippet-->
-</div>
-<!--Here ends the main contents-->
-
+<div id="gotop"><img src="images/gotop.png"></div>
 <footer>
     <div class="footer-top">
         <div class="container">
@@ -185,7 +77,7 @@ EOT;
 
                 <!--start foot logo-->
                 <div class="foot-logo col-sm-4 col-md-2 vertical-center">
-                    <img src="sources/foot_logo.png">
+                    <img src="images/foot_logo.png">
                 </div>
                 <!--end foot logo-->
 
@@ -216,7 +108,6 @@ EOT;
     </div>
     <!--end footer-bottom-->
 </footer>
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap.js"></script>
+<script src="js/script.js"></script>
 </body>
 </html>
---
+++ js/countnumber.js
@@ -0,0 +1,51 @@
+/**
+ * Created by shiwen on 2/23/2017.
+ */
+(function (global) {
+
+    function isShow($el) {
+        var winH = $(window).height(),//获取窗口高度
+            scrollH = $(window).scrollTop(),//获取窗口滚动高度
+            top = $el.offset().top;//获取元素距离窗口顶部偏移高度
+        if (top < scrollH + winH) {
+            return true;//在可视范围
+        } else {
+            return false;//不在可视范围
+        }
+    };
+
+    var alumni = $('#alumni-number').attr('count');
+    var visitors = $('#visitor-number').attr('count');
+    var fun = function () {
+        if (isShow($('#alumni-number'))) {
+            var i = 0;
+            var step1 = Math.floor(alumni / 200);
+            var count_1 = setInterval(function () {
+                $('#alumni-number').html(i.toLocaleString('fr-FR'));
+                i = i + step1;
+                if (i > alumni) {
+                    clearInterval(count_1);
+                    console.log(alumni);
+                    $('#alumni-number').html(parseInt(alumni, 10).toLocaleString('fr-FR'));
+
+                }
+            }, 2);
+
+            var j = 0;
+            var step2 = Math.floor(visitors / 200);
+            var count_2 = setInterval(function () {
+                $('#visitor-number').html(j.toLocaleString('fr-FR'));
+                j = j + step2;
+                if (j > visitors) {
+                    clearInterval(count_2);
+                    $('#alumni-visitor').html(parseInt(visitors, 10).toLocaleString('fr-FR'));
+                }
+            }, 2);
+
+            window.removeEventListener('scroll',fun);
+        }
+    }
+    window.addEventListener("scroll", fun);
+
+
+})(window);
